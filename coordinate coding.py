7b3a3f2b1996b96e52c6d6dc3b342574a7c75bf2
MaryamJamali <150019136+MaryaJamali@users.noreply.github.com>
2023-12-14 11:54:17 +0330

    Create coordinate coding.py

---
+++ coordinate coding.py
@@ -0,0 +1,12 @@
+# Convert the input number to float
+nx = float(input('Enter Original x in Flout Format: '))
+ny = float(input('Enter Original y in Flout Format: '))
+# We separate the decimal from the number and save the correct part of the number
+nxi = int(nx)
+nyi = int(ny)
+nxs = str(nx-nxi)
+nys = str(ny-nyi)
+enx = str(nxi**2-7)
+eny = str(nyi**3-6)
+print('Encrypted x is :'+' '+enx+'.'+nxs)
+print('Encrypted y is :'+' '+eny+'.'+nys)
